40255a8eacc9cb0bb7d5a121824deb592921b73f
kumar <kumarvermaankit9555@gmail.com>
2022-01-17 21:31:20 +0530

    final

--- src/blog.js
+++ src/blog.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useState } from 'react/cjs/react.development'
+import { useState } from 'react'
 import Tinymce from "./tinymce"
 import axios from 'axios';
 
